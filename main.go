0f49a7199100869d5d76cff4b2c132fb0dd4e1c0
Alan Bardales <alan.g.bardales@gmail.com>
2021-10-26 12:07:16 -0500

    Added custom function

--- main.go
+++ main.go
@@ -24,6 +24,11 @@ type shortenBody struct {
 	LongUrl string `json:"longUrl"`
 }
 
+type customBody struct {
+	LongUrl    string `json:"longUrl"`
+	CustomCode string `json:"customCode"`
+}
+
 type UrlDoc struct {
 	ID        primitive.ObjectID `bson:"_id"`
 	UrlCode   string             `bson:"urlCode"`
@@ -57,6 +62,7 @@ func main() {
 	})
 	r.GET("/:code", redirect)
 	r.POST("/shorten", shorten)
+	r.POST("/custom", custom)
 
 	r.Run(":5000")
 }
@@ -138,4 +144,64 @@ func redirect(c *gin.Context) {
 	log.Print(result["longUrl"])
 	var longUrl = fmt.Sprint(result["longUrl"])
 	c.Redirect(http.StatusPermanentRedirect, longUrl)
+}
+
+func custom(c *gin.Context) {
+	var body customBody
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, urlErr := url.ParseRequestURI(body.LongUrl)
+	if urlErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid URL"})
+		return
+	}
+	var length = len(body.CustomCode)
+	if length < 3 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Custom code should be more than 3 characters"})
+		return
+	}
+
+	var result bson.M
+	queryErr := collection.FindOne(ctx, bson.D{{"urlCode", body.CustomCode}}).Decode(&result)
+
+	if queryErr != nil {
+		if queryErr != mongo.ErrNoDocuments {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": queryErr.Error()})
+			return
+		}
+	}
+
+	if len(result) > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Code in use: %s", body.CustomCode)})
+		return
+	}
+
+	var date = time.Now()
+	var expires = date.AddDate(0, 0, 5)
+	var newUrl = baseUrl + body.CustomCode
+	var docId = primitive.NewObjectID()
+
+	newDoc := &UrlDoc{
+		ID:        docId,
+		UrlCode:   body.CustomCode,
+		LongUrl:   body.LongUrl,
+		ShortUrl:  newUrl,
+		CreatedAt: time.Now(),
+		ExpiresAt: expires,
+	}
+
+	_, err := collection.InsertOne(ctx, newDoc)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"newUrl":  newUrl,
+		"expires": expires.Format("2006-01-02 15:04:05"),
+		"db_id":   docId,
+	})
 }
